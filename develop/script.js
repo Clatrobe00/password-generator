2823a03cb87c76fef96b9337af339e00d87ad922
Charles Latrobe <Charlie.bateman117@gmail.com>
2020-11-18 08:46:48 -0800

    add passLength function to js.

--- develop/script.js
+++ develop/script.js
@@ -10,5 +10,25 @@ function writePassword() {
 
 }
 
+function generatePassword() {
+  passLength = howLong()
+
+  console.log(passLength);
+}
+
+function howLong() {
+  passLength = prompt("How many characters does your password need to have?");
+  while (passLength < 8 || passLength > 128) {
+    if (passLength < 8) {
+      alert("Your password must have at least 8 characters.")
+      passLength = prompt("How many characters does your password need to have?");
+      } else if (passLength > 128) {
+        alert("Your password must have fewer than 128 characters.")
+        passLength = prompt("How many characters does your password need to have?");
+        }
+    }
+  return passLength      
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
